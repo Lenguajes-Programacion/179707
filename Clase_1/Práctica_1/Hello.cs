bf2f5d098c4c2c0337421ae4461b112b3f0eae90
Daniela180 <60111274+Daniela180@users.noreply.github.com>
2020-01-22 12:59:15 -0700

    Hola mundo

---
+++ Clase_1/Práctica_1/Hello.cs
@@ -0,0 +1,23 @@
+using System;
+//Importamos la librería principal.
+//namespace manera de agrupar clases.
+
+namespace Practica_uno { 
+
+    class Program
+    {
+        static void Main (string args)
+
+        {
+            Console.WriteLine("Hola Mundo!");
+            
+
+        }      
+            
+            
+      }
+
+
+
+}
+
