d24b06626a515d9fe80e3a3f5b1173d5f8f8caec
DANIELA DEL ROSARIO CARRERA MEDINA <al179707@alumnos.uacj.mx>
2020-01-27 12:32:30 -0700

    Practica_uno

---
+++ Clase_1/Practica_uno/Practica_uno/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Practica_uno
+{
+    class Program
+    {
+        //Variable de la clase
+        static string nombre = "Daniela";
+
+        static void Main(string[] args)
+
+        {
+
+            //Respondemos con el primer argumento.
+            //Condición se debe cumplir todas las sentencias si utilizamos &&.
+            //Condición se debe cumplir cualquiera de las sentencias si utilizamos ||.
+            //Tipos de comparaciones > < == >= <= != diferente que.
+            if (args[0].Length > 0 && args[0] == nombre)// true or false
+            {
+                Console.WriteLine($"Hello{args[0]}{args[1]}");
+            }
+            else
+            {
+                Console.WriteLine("User unauthorized");
+            }
+        }
+    }
+}
